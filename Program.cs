f1648d1515a652db84862a998dce62631c1dc15b
Jessica <jpanndora@gmail.com>
2025-09-19 00:41:04 -0300

    Atividade concluída

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AMOR- Haikai Brasileiro");
+Console.WriteLine("-----------------------");
+Console.WriteLine("Pra falar de amor ");
+Console.WriteLine("Nem precisa de flor ");
+Console.WriteLine("Basta o coração");
+Console.WriteLine("-----------------------");
+Console.WriteLine("Amo-te tanto assim ");
+Console.WriteLine("Nem sei se é amor ");
+Console.WriteLine("Ou só paixão");
+Console.WriteLine("-----------------------");
